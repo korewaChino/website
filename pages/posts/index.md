72e0055d20ec7532879aaf10d615cd620650e951
Cappy Ishihara <cappy@cappuchino.xyz>
2023-12-19 20:57:11 +0700

    Initial commit

---
+++ pages/posts/index.md
@@ -0,0 +1,10 @@
+---
+type: posts
+title: Blogs
+date: 2021-03-18
+---
+
+# Blogs
+
+<h1 className='title2'>Recent Posts</h1>
+
---
+++ theme.config.jsx
@@ -0,0 +1,59 @@
+const YEAR = new Date().getFullYear()
+import {
+  FaHandshake
+} from 'react-icons/fa'
+import {
+  BsChatSquareDotsFill
+
+} from 'react-icons/bs'
+import CustomHead from './components/custom-head'
+
+
+export default {
+
+  head: CustomHead,
+
+  footer: (
+    <small style={{ fontSize: '14px',display:'block',marginTop:'6rem',display:'flex' }}>
+      <span><time>{YEAR}</time> © Ninad Vyas</span>
+
+        <div className='footicon'>
+          <a href="https://www.linkedin.com/in/ninad-vyas-b767491a3/" className='footiconin'>
+           <a className='icon1'>
+            <FaHandshake
+              style={{
+                color: '#7g7g7f',
+                fontSize: '18.5px',
+                width: '30px',
+                marginTop:'3px'
+              }}
+            />
+          </a>Stay Connected!</a>
+
+
+         
+        </div>
+
+      <style jsx>{`
+        a {
+          float: right;
+        }
+        @media screen and (max-width: 480px) {
+          article {
+            padding-top: 2rem;
+            padding-bottom: 4rem;
+          }
+        }
+      `}</style>
+    </small>
+  ), 
+  components: {
+    logo: (
+      <>
+        <span>Ninad Vyas</span>
+      </>
+    ),
+    titleSuffix: "Ninad Vyas ",
+  },
+  darkMode: false
+}
